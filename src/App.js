4902ca3d64e00d3008533a073d6655b4399a1ed7
Ridkokasha Liubov <127142325+liubovridko@users.noreply.github.com>
2023-08-10 11:30:23 +0300

    Merge branch Developer into Dev (#14)
    * Add slider -home page
    
    * add alt for img
    
    * finished home page layout
    
    * Merge changes from remote
    
    * add cart layout
    
    * Add components
    
    * Add component Slider
    
    * create scss modules
    
    * create handle click on plus card and favorite btn
    
    * storing and accessing data on the server side using axios
    
    * Add favorite list plus react router dom
    
    ---------
    
    Co-authored-by: Liubov <liubovusikova1@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import React from 'react';
 import './App.scss';
 import axios from 'axios';
-import Card from './components/Card/Card.js';
+
+import {Route, Routes} from 'react-router-dom';
 import Header from './components/Header/Header.js';
 import Drawer from './components/Drawer/Drawer.js';
-import Slider from './components/Slider/Slider.js';
+import Home from './pages/Home.jsx';
+import Favorites from './pages/Favorites.jsx';
 
 
 function App() {
@@ -26,6 +28,7 @@ function App() {
 
   const [items, setItems] = React.useState([]);
   const [itemsCart, setItemsCart] = React.useState([]);
+  const [itemsFavorite, setItemsFavorite] = React.useState([]);
   const [searchValue, setSearchValue] = React.useState("");
   const [cartOpened, setCartOpened] = React.useState(false);
 
@@ -42,13 +45,29 @@ function App() {
       setItemsCart(res.data);
      });
 
+     axios.get('https://react-shop.free.mockoapp.net/favorite').then((res) =>{
+      setItemsFavorite(res.data);
+     });
+
 
   }, []);
 
   const onAddToCart = (obj) => {
      axios.post('https://64d11d88ff953154bb7a01ae.mockapi.io/cart', obj);
      setItemsCart( prev => [...prev, obj]);
-  };
+  }
+  const addToFavorite = (obj) => {
+    console.log(obj);
+
+    if(itemsFavorite.find(obj => obj.id == obj.id)) {
+      axios.delete(`https://react-shop.free.mockoapp.net/favorite/${obj.id}`);
+      setItemsFavorite( prev => prev.filter((item) => item.id !== obj.id));
+      } else {
+      axios.post('https://react-shop.free.mockoapp.net/favorite', obj);
+      setItemsFavorite( prev => [...prev, obj]);     
+     }
+      
+  }
 
   const onRemoveItem = (id)=> {
     axios.delete(`https://64d11d88ff953154bb7a01ae.mockapi.io/cart/${id}`);
@@ -57,41 +76,20 @@ function App() {
 
   const onChangeSearchValue= (event) => {
       setSearchValue(event.target.value);
-  };
+  }
 
   return (
     <div className="wrapper clear">
      
     { cartOpened && <Drawer items={itemsCart} onClose={()=>setCartOpened(false)} onRemove ={onRemoveItem} />  }
-
-    <Header  onClickCart={()=> setCartOpened(true)} />
     
-   <Slider />
+    <Header  onClickCart={()=> setCartOpened(true)} />
+    <Routes>
+      <Route path="/" exact element={<Home searchValue={searchValue} onChangeSearchValue={onChangeSearchValue} items={items} onAddToFavorite={(obj)=>addToFavorite(obj)}  onAddToCart={onAddToCart}  />} />
+       
+      <Route path="/favorites" element={ <Favorites items={itemsFavorite} onAddToFavorite={(obj)=>addToFavorite(obj)}  onAddToCart={onAddToCart} />} exact />          
+    </Routes>
 
-    <section className="content">
-
-        <div  className="d-flex justify-between mb-30">
-
-        <h1>{searchValue ? `Пошук по значенню:"${searchValue}"` : "Усі кроссовки" }</h1>
-          <div className="search-form d-flex">
-           <img src="img/search.svg" alt="search" />
-            <input onChange={onChangeSearchValue} className="input_search" type="search" value={searchValue} placeholder="Пошук..." />
-
-          </div>  
-        
-        </div>
-        <div className="d-flex justify-between flex-wrap">
-
-     {items
-        .filter(item=> item.title.toLowerCase().includes(searchValue.toLowerCase()))
-        .map((item, index) => (
-              <Card key={index} title={item.title} price={item.price} imageUrl={item.imageUrl} onClickFavorite={()=> console.log('Add to favorite')} onPlus={(obj) => onAddToCart(obj)} />
-      ))}
-           
-            
-          </div>
-                             
-    </section>
       
     </div>
   );
